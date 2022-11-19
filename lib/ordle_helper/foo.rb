da7ea0b0154c51907d6483d8a79af36280942dee
Johnny Allen <johnny.allen@daveramsey.com>
2022-11-19 12:04:35 -0600

    full functionality

--- lib/ordle_helper/foo.rb
+++ lib/ordle_helper/foo.rb
@@ -1,13 +1,20 @@
 module OrdleHelper
   class Foo
     WORD_BANK = "word_bank.csv"
-    NOT_INCLUDED_LETTERS = %w(r e c h i l d k)
+    NOT_INCLUDED_LETTERS = %w()
     CORRECT_LETTERS = {
-      0 => nil,
-      1 => nil,
-      2 => "a",
-      3 => nil,
-      4 => "t"
+      0 => "",
+      1 => "",
+      2 => "",
+      3 => "",
+      4 => ""
+    }
+    INCLUDED_LETTERS_WRONG_SPOT = {
+      0 => %w(),
+      1 => %w(),
+      2 => %w(),
+      3 => %w(),
+      4 => %w(),
     }
 
     attr_accessor :word_bank
@@ -16,13 +23,17 @@ module OrdleHelper
     end
 
     def call
-      exclude_not_included_letters
+      exclude_words_with_not_included_letters
       exclude_words_without_correct_letters
-      binding.pry
+      exclude_words_without_included_letters_in_wrong_spot
+      puts "Possible words:"
+      word_bank.each do |word|
+        puts "\t#{word}"
+      end
       true
     end
 
-    def exclude_not_included_letters
+    def exclude_words_with_not_included_letters
       NOT_INCLUDED_LETTERS.each do |letter|
         word_bank.reject! { |word| word.include?(letter) }
       end
@@ -30,8 +41,20 @@ module OrdleHelper
 
     def exclude_words_without_correct_letters
       CORRECT_LETTERS.each do |position, letter|
-        next if letter.nil?
+        next if letter.empty?
+
         word_bank.select! { |word|  word[position] == letter }
+      end
+    end
+
+    def exclude_words_without_included_letters_in_wrong_spot
+      INCLUDED_LETTERS_WRONG_SPOT.each do |position, letters|
+        next if letters.empty?
+
+        letters.each do |letter|
+          word_bank.reject! { |word| word[position] == letter }
+          word_bank.select! { |word| word.include?(letter) }
+        end
       end
     end
   end
