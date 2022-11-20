29a6c26172163d89599ae5f8ec5dff6af43c6991
Johnny Allen <johnny.allen@daveramsey.com>
2022-11-20 14:18:44 -0600

    more error handling

--- run.rb
+++ run.rb
@@ -1,8 +1,8 @@
 require_relative "lib/ordle_helper"
 
 winning_words = OrdleHelper::WinningWord.new
-new_winners = ENV["WINNING_WORDS"]&.split&.compact
+new_winners = ENV["WINNING_WORDS"]&.split&.compact || []
 new_winners.each do |winner|
   winning_words.add_winner(winner)
 end
-winning_words.print_winners
+winning_words.print_winners if ENV["PRINT_WINNERS"] == "1"
