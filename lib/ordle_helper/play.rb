b4926aa7f0f2e237dcb7509e24e2cb3a5b6c9770
Johnny Allen <johnny.allen@daveramsey.com>
2022-12-03 16:20:44 -0600

    make games and finished games eagerly loaded instance variables in play

--- lib/ordle_helper/play.rb
+++ lib/ordle_helper/play.rb
@@ -1,12 +1,15 @@
 module OrdleHelper
   class Play
     MAX_NUMBER_OF_TURNS = 13
+    EXIT_WORDS = %w(DONE QUIT EXIT STOP).freeze
 
-    attr_reader :input, :output
+    attr_reader :input, :output, :games, :finished_games
 
     def initialize(input: $stdin, output: $stdout)
       @input = input
       @output = output
+      @games = []
+      @finished_games = []
     end
 
     def determine_and_initiate_game
@@ -30,24 +33,29 @@ module OrdleHelper
 
     def call(game_instances = 1)
       output.puts "Initiating word finder.".light_blue
-      games = []
       game_instances.times { |i| games[i] = OrdleHelper::WordFinder.new }
-      finished_games = []
 
       MAX_NUMBER_OF_TURNS.times do |_turn|
         output.puts "Enter your guess. If you would like to exit, type 'done' and press Enter/Return.".light_blue
         user_input = gets.chomp.upcase
-        return if %w(DONE QUIT EXIT STOP).include?(user_input)
+        return if EXIT_WORDS.include?(user_input)
         raise "#{user_input} is not a valid word" unless OrdleHelper::WordFinder.word_bank_contains?(user_input)
-
-        games.each_with_index do |game, index|
-          next if finished_games.include?(game)
-
-          finished_games << game if game.add_guess(word: user_input, game_number: index + 1) == "DONE"
-        end
-
-        return if games.size == finished_games.size
+        add_guess_to_games(user_input)
+        return if all_games_completed?
       end
+    end
+
+    def add_guess_to_games(word)
+      games.each_with_index do |game, index|
+        next if finished_games.include?(game)
+        game_number = index + 1
+
+        finished_games << game if game.add_guess(word:, game_number:) == "DONE"
+      end
+    end
+
+    def all_games_completed?
+      games.size == finished_games.size
     end
   end
 end
