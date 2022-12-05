1fc81cea3ce8a66f7166c429592bbacd07426d71
Johnny Allen <johnny.allen@daveramsey.com>
2022-12-05 07:54:33 -0600

    Allow more than ten words if potential plurals included

--- lib/ordle_helper/word_finder.rb
+++ lib/ordle_helper/word_finder.rb
@@ -186,7 +186,7 @@ module OrdleHelper
 
     def output_current_word_bank_state
       puts "#{word_bank.size} possible words:"
-      return if word_bank.size > 10
+      return if word_bank.reject { |word| potential_plural?(word) }.size > 10
 
       word_bank.each do |word|
         if potential_plural?(word)
