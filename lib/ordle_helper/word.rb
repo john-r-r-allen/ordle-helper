53486b9d0a0f5e2f750e700981d19e1e606d9d42
Johnny Allen <johnny.allen@daveramsey.com>
2022-12-08 14:08:46 -0600

    Add Word class

--- lib/ordle_helper/word.rb
+++ lib/ordle_helper/word.rb
@@ -3,9 +3,15 @@ module OrdleHelper
     CORRECT_LETTER = "G".freeze
     RIGHT_LETTER_WRONG_SPOT = "Y".freeze
     NOT_INCLUDED_LETTER = "N".freeze
+    GUESSED_CORRECT_WORD = "GGGGG".freeze
     RESET_CORRECT_LETTER_ERROR = "Attempting to reset correct letter to new value".freeze
+    WORD_SIZE = 5
 
-    attr_reader :guesses, :not_included_letters, :correct_letters, :included_letters_wrong_spot
+    attr_reader :guesses,
+                :not_included_letters,
+                :correct_letters,
+                :included_letters_wrong_spot,
+                :included_letters_with_known_number_of_occurrences
 
     def initialize
       @guesses = []
@@ -24,21 +30,52 @@ module OrdleHelper
         3 => %w(),
         4 => %w()
       }
+      @included_letters_with_known_number_of_occurrences = {}
     end
 
     def add_guess(guess:, guess_feedback:)
-      handle_multiple_letter_occurences(guess:, guess_feedback:)
+      raise "Incomplete guess feedback" unless guess_feedback.size == WORD_SIZE
+      return "DONE" if guess_feedback == GUESSED_CORRECT_WORD
+
+      handle_multiple_letter_occurrences(guess:, guess_feedback:) if guess_contains_duplicated_letters?(guess)
       guess_feedback.split("").each_with_index do |feedback, index|
-        case feedback
-        when CORRECT_LETTER
-          add_to_correct_letters(guess:, index:)
-        when RIGHT_LETTER_WRONG_SPOT
-          add_to_included_letters_wrong_spot(guess:, index:)
-        when NOT_INCLUDED_LETTER
-          add_to_not_included_letters(guess:, index:)
-        else
-          raise "Invalid response. The only valid responses are 'N', 'Y', and 'G'."
+        process_feedback(guess:, feedback:, index:)
+      end
+    end
+
+    def guess_contains_duplicated_letters?(guess)
+      guess.split("").tally.values.max != 1
+    end
+
+    def handle_multiple_letter_occurrences(guess:, guess_feedback:)
+      guess_array = guess.split("")
+      guess_array.each_with_index do |letter, index|
+        next if guess_array.tally[letter] == 1
+        next unless guess_array.index(letter) == index # only take action on first instance of the letter
+
+        position_and_feedback = { index => guess_feedback[index] }
+        ((index + 1)..WORD_SIZE).each do |i|
+          position_and_feedback[i] = guess_feedback[i] if guess[i] == letter
         end
+        next if position_and_feedback.values.uniq.size == 1 # no action for all the same feedback
+        next if position_and_feedback.values.uniq.sort == %w(G Y)
+        next if included_letters_with_known_number_of_occurrences.key?(letter)
+
+        @included_letters_with_known_number_of_occurrences[letter] =
+          position_and_feedback.reject { |_position, feedback| feedback == "N" }.size
+      end
+    end
+
+    def process_feedback(guess:, feedback:, index:)
+      case feedback
+      when CORRECT_LETTER
+        add_to_correct_letters(guess:, index:)
+      when RIGHT_LETTER_WRONG_SPOT
+        add_to_included_letters_wrong_spot(guess:, index:)
+      when NOT_INCLUDED_LETTER
+        add_to_not_included_letters(guess:, index:)
+      else
+        raise "Invalid response. The only valid responses are 'N', 'Y', and 'G'."
       end
     end
 
--- spec/lib/ordle_helper/word_spec.rb
+++ spec/lib/ordle_helper/word_spec.rb
@@ -22,10 +22,166 @@ RSpec.describe OrdleHelper::Word do
   end
 
   describe "#add_guess" do
+    context "when the guess_feedback provided is not the right size" do
+      let(:guess_feedback) { "NYG" }
+      it "raises an error and does not call process_feedback" do
+        expect(subject).not_to receive(:process_feedback)
 
+        expect { subject.add_guess(guess:, guess_feedback:) }.to raise_error("Incomplete guess feedback")
+      end
+    end
+
+    context "when the guess_feedback provided is the right size" do
+      context "when the guess is the correct word" do
+        let(:guess_feedback) { "GGGGG" }
+
+        it "returns 'DONE' without calling any other methods" do
+          expect(subject).not_to receive(:guess_contains_duplicated_letters?)
+          expect(subject).not_to receive(:handle_multiple_letter_occurrences)
+          expect(subject).not_to receive(:process_feedback)
+
+          expect(subject.add_guess(guess:, guess_feedback: )).to eq("DONE")
+        end
+      end
+
+      context "when the guess is not the correct word" do
+        let(:guess_feedback) { "NNNYG" }
+        context "when the guess does not contain any letter more than once" do
+          it "does not call handle_multiple_letter_occurrences and calls process_feedback five times" do
+            expect(subject).to receive(:guess_contains_duplicated_letters?).with(guess).and_return(false).once
+            expect(subject).not_to receive(:handle_multiple_letter_occurrences)
+            expect(subject).to receive(:process_feedback).exactly(5).times
+
+            subject.add_guess(guess:, guess_feedback:)
+          end
+        end
+
+        context "when the guess does contain a letter more than once" do
+          let(:guess) { "SHEET" }
+
+          it "calls handle_multiple_letter_occurrences and calls process_feedback five times" do
+            expect(subject).to receive(:guess_contains_duplicated_letters?).with(guess).and_return(true).once
+            expect(subject).to receive(:handle_multiple_letter_occurrences).with({ guess:, guess_feedback: }).once
+            expect(subject).to receive(:process_feedback).exactly(5).times
+
+            subject.add_guess(guess:, guess_feedback:)
+          end
+        end
+      end
+    end
   end
 
-  fdescribe "#add_to_correct_letters" do
+  describe "#handle_multiple_letter_occurrences" do
+    let(:guess) { "SHEET" }
+
+    context "when the letter(s) with multiple occurrences have the same feedback" do
+      let(:guess_feedback) { "GNGGG" }
+
+      it "does not set an occurrence limit" do
+        expect(subject.instance_variable_get(:@included_letters_with_known_number_of_occurrences)).to eq({})
+
+        subject.handle_multiple_letter_occurrences(guess:, guess_feedback:)
+
+        expect(subject.instance_variable_get(:@included_letters_with_known_number_of_occurrences)).to eq({})
+      end
+    end
+
+    context "when the letter(s) with multiple occurrences have differing feedback" do
+      context "when all pieces of feedback indicate the letter is included ('Y' or 'G')" do
+        let(:guess_feedback) { "GNYGN" }
+
+        it "does not set an occurrence limit" do
+          expect(subject.instance_variable_get(:@included_letters_with_known_number_of_occurrences)).to eq({})
+
+          subject.handle_multiple_letter_occurrences(guess:, guess_feedback:)
+
+          expect(subject.instance_variable_get(:@included_letters_with_known_number_of_occurrences)).to eq({})
+        end
+      end
+
+      context "when pieces of feedback indicate the letter is included ('Y' or 'G') and not included ('N')" do
+        let(:guess_feedback) { "GNGNN" }
+
+        context "when the letter does not already have an occurrence limit set" do
+          it "sets the occurrence limit for letters appearing more than once" do
+            expect(subject.instance_variable_get(:@included_letters_with_known_number_of_occurrences)).to eq({})
+
+            subject.handle_multiple_letter_occurrences(guess:, guess_feedback:)
+
+            expect(subject.instance_variable_get(:@included_letters_with_known_number_of_occurrences)).to(
+              eq({ "E" => 1 })
+            )
+          end
+        end
+
+        context "when the letter already has an occurrence limit set" do
+          it "does not modify the occurrence limit recorded" do
+            subject.instance_variable_set(:@included_letters_with_known_number_of_occurrences, { "E" => 1 })
+
+            subject.handle_multiple_letter_occurrences(guess:, guess_feedback:)
+
+            expect(subject.instance_variable_get(:@included_letters_with_known_number_of_occurrences)).to(
+              eq({ "E" => 1 })
+            )
+          end
+        end
+      end
+    end
+  end
+
+  describe "#process_feedback" do
+    context "when the feedback is `G`" do
+      let(:feedback) { "G" }
+
+      it "calls add_to_correct_letters" do
+        expect(subject).to receive(:add_to_correct_letters).with({ guess:, index: }).once
+        expect(subject).not_to receive(:add_to_included_letters_wrong_spot)
+        expect(subject).not_to receive(:add_to_not_included_letters)
+
+        subject.process_feedback(guess:, feedback:, index:)
+      end
+    end
+
+    context "when the feedback is `Y`" do
+      let(:feedback) { "Y" }
+
+      it "calls add_to_included_letters_wrong_spot" do
+        expect(subject).not_to receive(:add_to_correct_letters)
+        expect(subject).to receive(:add_to_included_letters_wrong_spot).with({ guess:, index: }).once
+        expect(subject).not_to receive(:add_to_not_included_letters)
+
+        subject.process_feedback(guess:, feedback:, index:)
+      end
+    end
+
+    context "when the feedback is `N`" do
+      let(:feedback) { "N" }
+
+      it "calls add_to_not_included_letters" do
+        expect(subject).not_to receive(:add_to_correct_letters)
+        expect(subject).not_to receive(:add_to_included_letters_wrong_spot)
+        expect(subject).to receive(:add_to_not_included_letters).with({ guess:, index: }).once
+
+        subject.process_feedback(guess:, feedback:, index:)
+      end
+    end
+
+    context "when the feedback is not 'N', 'Y', or 'G'" do
+      let(:feedback) { "X" }
+
+      it "raises an error" do
+        expect(subject).not_to receive(:add_to_correct_letters)
+        expect(subject).not_to receive(:add_to_included_letters_wrong_spot)
+        expect(subject).not_to receive(:add_to_not_included_letters)
+
+        expect { subject.process_feedback(guess:, feedback:, index:) }.to(
+          raise_error("Invalid response. The only valid responses are 'N', 'Y', and 'G'.")
+        )
+      end
+    end
+  end
+
+  describe "#add_to_correct_letters" do
     context "when the correct letter is already set at that position" do
       context "when the set value matches the value attempting to be set" do
         it "it returns successfully" do
@@ -59,7 +215,7 @@ RSpec.describe OrdleHelper::Word do
     end
   end
 
-  fdescribe "#add_to_included_letters_wrong_spot" do
+  describe "#add_to_included_letters_wrong_spot" do
     context "when the letter is already in the included_letters_wrong_spot instance variable hash at that index" do
       it "does not add the letter" do
         subject.instance_variable_set(
@@ -90,7 +246,7 @@ RSpec.describe OrdleHelper::Word do
     end
   end
 
-  fdescribe "#add_to_not_included_letters" do
+  describe "#add_to_not_included_letters" do
     context "when the letter is already in the not_included_letters instance variable" do
       it "does not add the letter again" do
         subject.instance_variable_set(:@not_included_letters, ["H"])
